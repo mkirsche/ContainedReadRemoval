23593d242c2b761d5dd3426f59502173131bc7c6
Michael Kirsche <jnredotter@gmail.com>
2019-01-08 15:58:10 -0500

    Added script for looking at read lengths

---
+++ FastqLengthHist.py
@@ -0,0 +1,23 @@
+# Generates a histogram of read lengths from a file in FASTQ format
+import sys
+import matplotlib.pyplot as plt
+
+if len(sys.argv) != 3:
+    print 'Usage: python FastqLengthHist.py <readsfile> <outputfile>'
+    sys.exit()
+    
+fn = sys.argv[1]
+ofn = sys.argv[2]
+
+lens = []
+
+with open(fn) as f:
+    idx = 0
+    for line in f:
+        idx += 1
+        if idx%4 == 2:
+            lens.append(len(line))
+            
+plt.hist(lens)
+plt.savefig(ofn)
+            
