f6421387644819797266338cf1c3f7f323642c3d
Michael Kirsche <jnredotter@gmail.com>
2019-01-14 11:31:21 -0500

    Added call to eval.sh to work from outside directory

--- eval_all.sh
+++ eval_all.sh
@@ -1,9 +1,12 @@
 assemblydir=$1
 reffile=$2
 buscolineage=$3
+
+BINDIR=`dirname $(readlink -f "$0")`
+
 for i in `ls $assemblydir`
 do
     echo 'Evaluating ' $i
-    ./eval.sh $assemblydir/$i $reffile $i_stats -l $buscolineage
+    $BINDIR/eval.sh $assemblydir/$i $reffile $i_stats -l $buscolineage
 done
 
