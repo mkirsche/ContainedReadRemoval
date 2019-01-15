4dab161d08b23ebfd680bfaa370021dc2b90bce0
Michael Kirsche <jnredotter@gmail.com>
2019-01-15 08:44:09 -0500

    Bug fix

--- eval_all.sh
+++ eval_all.sh
@@ -3,6 +3,7 @@ reffile=$2
 buscolineage=$3
 
 BINDIR=`dirname $(readlink -f "$0")`
+OUTDIR=`pwd`
 
 ls $OUTDIR/inserts/ | parallel --gnu --timeout 500 --jobs 16 $BINDIR/eval.sh $assemblydir'/'{} $reffile {}'_stats' -l $buscolineage
 
