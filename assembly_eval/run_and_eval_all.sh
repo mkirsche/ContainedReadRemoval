330ec8088d0ce31de6e0ff7213f33504cb00833e
Michael Kirsche <jnredotter@gmail.com>
2019-01-17 15:06:22 -0500

    Added flags for skipping steps

--- assembly_eval/run_and_eval_all.sh
+++ assembly_eval/run_and_eval_all.sh
@@ -3,6 +3,9 @@ WORKINGDIR=`pwd`
 javac $BINDIR/*.java
 javac $BINDIR/../src/*.java
 
+skipfilter=0
+skipassembly=0
+
 usage() 
 { 
     echo "Usage: $0 -r <readfile> -g <refgenome> -t <readtype> -p <paramsfile> -o <outdir> -b <buscolineage> [-c -l <genome length>]" 1>&2;
@@ -15,7 +18,7 @@ usage()
 
 assembler='wtdbg2'
 
-while getopts r:g:t:p:c:l:o:b: option
+while getopts r:g:t:p:c:l:o:b:skipfilter:skipassembly: option
 do
     case "${option}"
         in
@@ -27,8 +30,14 @@ do
         p) paramsfile=${OPTARG};;
         g) ref=${OPTARG};;
         b) buscolineage=${OPTARG};;
+        skipfilter) skipfilter=1;;
+        skipassembly) skipassembly=1;;
     esac
 done
+
+if [ $skipassembly -eq 1 ]; then
+    skipfilter=1
+fi
 
 echo 'readfile: '$readfile
 echo 'readtype: '$readtype
@@ -55,37 +64,41 @@ mkdir $outdir'/stats'
 cp $ref $outdir'/assemblies/ref.fa'
 
 # Iterate over sets of parameters and perform filtering for each
-while read p; 
-do 
-    echo $p;
-    if [ "$readtype" = "ccs" ]; then
-        newreadsfile=`../src/filter_ccs.sh $readfile $p`
-    else
-        newreadsfile=`../src/filter_pb_np.sh $readfile $p`
-    fi
-    
-    if [ -e $newreadsfile ]; then
-        mv $newreadsfile $outdir'/readsets'
-    fi
-done < $paramsfile
+if [ -z "${skipfilter}" ]; then
+    while read p; 
+    do 
+        echo $p;
+        if [ "$readtype" = "ccs" ]; then
+            newreadsfile=`../src/filter_ccs.sh $readfile $p`
+        else
+            newreadsfile=`../src/filter_pb_np.sh $readfile $p`
+        fi
+        
+        if [ -e $newreadsfile ]; then
+            mv $newreadsfile $outdir'/readsets'
+        fi
+    done < $paramsfile
+fi
 
 cd $outdir'/assemblyruns'
 
 # Go through readsets and perform an assembly for each
-for in in `ls $outdir'/readsets'`; do
-    if [ "$assembler" = "canu" ]; then
-        if [ "$readtype" = "ccs" ]; then
-            crt='pacbio-corrected'
-        elif [ "$readtype" = "pacbio" ]; then
-            crt='pacbio-raw'
+if [ -z "${skipassembly}" ]; then
+    for in in `ls $outdir'/readsets'`; do
+        if [ "$assembler" = "canu" ]; then
+            if [ "$readtype" = "ccs" ]; then
+                crt='pacbio-corrected'
+            elif [ "$readtype" = "pacbio" ]; then
+                crt='pacbio-raw'
+            else
+                crt='nanopore-raw'
+            fi
+            $BINDIR/../src/assemble.sh -r $outdir'/readsets/'$i -o $i -c -t $crt -l $length
         else
-            crt='nanopore-raw'
+            $BINDIR/../src/assemble.sh -r $outdir'/readsets/'$i -o $i
         fi
-        $BINDIR/../src/assemble.sh -r $outdir'/readsets/'$i -o $i -c -t $crt -l $length
-    else
-        $BINDIR/../src/assemble.sh -r $outdir'/readsets/'$i -o $i
-    fi
-done
+    done
+fi
 
 cd $WORKINGDIR
 
