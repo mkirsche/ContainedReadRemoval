2bd1249346fa2c073bed516e990fce94fba8ffc7
Michael Kirsche <jnredotter@gmail.com>
2019-04-10 10:06:52 -0400

    Added filter to overall script

--- sim/build.sh
+++ sim/build.sh
@@ -2,3 +2,5 @@ BINDIR=`dirname $(readlink -f "$0")`
 javac -cp "$BINDIR:$BINDIR/commons-math3-3.6.1.jar" SimulateReadsFromGenome.java 
 
 java -cp "$BINDIR:$BINDIR/commons-math3-3.6.1.jar" SimulateReadsFromGenome coverage=30 maxLength=5000000
+
+java -cp ../src PB_FilterContainedReads simulatedreads.fa nt=2 ct=.022 ofn=uncontained.txt k1=15
