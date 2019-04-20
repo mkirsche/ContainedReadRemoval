c9f5cee7301a1862c92c9c48a61eda077371497c
mkirsche <jnredotter@gmail.com>
2019-04-19 21:12:47 -0400

    Merge

--- sim/build.sh
+++ sim/build.sh
@@ -1,7 +1,7 @@
 BINDIR=`dirname $(readlink -f "$0")`
 javac -cp "$BINDIR:$BINDIR/commons-math3-3.6.1.jar" SimulateReadsFromGenome.java 
 
-java -cp "$BINDIR:$BINDIR/commons-math3-3.6.1.jar" SimulateReadsFromGenome coverage=30 maxLength=5000000 error=0.12 sample=ERsample.fastq
+java -cp "$BINDIR:$BINDIR/commons-math3-3.6.1.jar" SimulateReadsFromGenome coverage=30 maxLength=50000000 error=0.12 sample=../../ERR2173373.fastq
 
 #java -cp "$BINDIR:$BINDIR/commons-math3-3.6.1.jar" SimulateReadsFromGenome coverage=30 maxLength=5000000 error=0.05 sample=ERsample.fastq
 
