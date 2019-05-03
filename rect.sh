8857aa1cc7bbf977ffcf3eee6e519c4c336bbab6
Michael Kirsche <jnredotter@gmail.com>
2019-05-03 15:13:14 -0400

    Updated script to log real data

--- rect.sh
+++ rect.sh
@@ -1,7 +1,8 @@
 len=$1
 ct=$2
-name=$3
-javac src/*.java; time java -cp src PB_FilterContainedReads ../ERR2173373.fastq nt=24 ct=$ct w1=5 ofn=/home-3/mkirsche@jhu.edu/hashing/CCS/results/$name.txt dfn=/home-3/mkirsche@jhu.edu/hashing/CCS/results/$name.debug.txt k1=15 rt=.6 lf=$len method=rectangle  2>&1 | tee /home-3/mkirsche@jhu.edu/hashing/CCS/results/$name.log.txt
+et=$3
+name=$4
+javac src/*.java; time java -cp src PB_FilterContainedReads ../ERR2173373.fastq nt=24 ct=$ct et=$et w1=5 ofn=/home-3/mkirsche@jhu.edu/hashing/CCS/results/$name.txt dfn=/home-3/mkirsche@jhu.edu/hashing/CCS/results/$name.debug.txt k1=15 rt=.6 lf=$len method=rectangle logging  2>&1 | tee /home-3/mkirsche@jhu.edu/hashing/CCS/results/$name.log.txt
 
 java -cp /home-3/mkirsche@jhu.edu/hashing/CCS/src ExtractReads /home-3/mkirsche@jhu.edu/hashing/CCS/results/$name.txt /home-3/mkirsche@jhu.edu/hashing/ERR2173373.fastq
 
